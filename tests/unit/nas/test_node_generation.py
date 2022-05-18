8ff5a22eb1cc494e66bb08467c4c8e71a4c662d6
staeros <staeros22@gmail.com>
2022-05-18 19:55:29 +0300

    PEP 8 fixes

--- tests/unit/nas/test_node_generation.py
+++ tests/unit/nas/test_node_generation.py
@@ -8,10 +8,10 @@ from nas.graph_cnn_gp_operators import create_dropout_node, create_secondary_nod
 #  for checking is generated graph is valid(params and nodes) for both static and random graphs
 #  (instead of compare nodes compare graphs);
 #  for mutations(is new params valid);
+#  for validation rules;
 #  for load images func(pickle and folder cases);
 #  Implement ability to turn on/off skip connections generation;
 #  Rework skip connection generation
 def test_dropout_node():
     generated_node = create_dropout_node(NNNode)
     print("!")
-
