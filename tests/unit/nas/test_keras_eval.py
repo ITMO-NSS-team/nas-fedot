89b4e5ad7a97a1e53c14b778bc22f368ca2b4905
staeros <staeros22@gmail.com>
2022-06-20 20:23:08 +0300

    Unit tests update

--- tests/unit/nas/test_keras_eval.py
+++ tests/unit/nas/test_keras_eval.py
@@ -4,17 +4,27 @@ from nas.composer.cnn_graph import CNNGraph
 from nas.composer.cnn_graph_operator import random_conv_graph_generation
 from nas.nn.graph_keras_eval import create_nn_model
 from nas.utils.utils import project_root
+from nas.utils.var import DEFAULT_NODES_PARAMS
+from nas.cnn_builder import NASDirector, CNNBuilder
 
 root = project_root()
-requirements = GPNNComposerRequirements(image_size=[120, 120], init_graph_with_skip_connections=True)
-NODES_LIST = ['conv2d', 'conv2d', 'dropout', 'conv2d', 'conv2d', 'conv2d', 'flatten', 'dense', 'dropout',
-              'dense', 'dense']
+requirements = GPNNComposerRequirements(input_shape=[120, 120, 3], pop_size=1,
+                                        num_of_generations=1, max_num_of_conv_layers=4,
+                                        max_nn_depth=3, primary=['conv2d'], secondary=['dense'],
+                                        batch_size=4, epochs=1,
+                                        has_skip_connection=True, skip_connections_id=[0, 2, 5], shortcuts_len=2,
+                                        batch_norm_prob=-1, dropout_prob=-1,
+                                        default_parameters=DEFAULT_NODES_PARAMS)
+NODES_LIST = ['conv2d', 'conv2d', 'conv2d', 'conv2d', 'conv2d', 'flatten', 'dense', 'dense', 'dense']
 
 
 def generate_graphs():
+    director = NASDirector()
+    director.set_builder(CNNBuilder(NODES_LIST, requirements=requirements))
     graphs = []
     for _ in range(10):
-        graphs.append(random_conv_graph_generation(CNNGraph, CNNNode, requirements))
+        graph = director.create_nas_graph()
+        graphs.append(graph)
     return graphs
 
 
@@ -23,7 +33,7 @@ def test_is_trainable_model():
     success = True
     for graph in graphs:
         try:
-            create_nn_model(graph, [120, 120, 3], 3)
+            create_nn_model(graph, [120, 120, 3], 4)
         except ValueError:
             success = False
     assert success
--- tests/unit/nas/test_validation.py
+++ tests/unit/nas/test_validation.py
@@ -2,15 +2,24 @@ import os
 
 from nas.graph_cnn_mutations import has_no_flatten_skip, graph_has_several_starts, flatten_check, \
     graph_has_wrong_structure
+from nas.composer.gp_cnn_composer import GPNNComposerRequirements
 from nas.composer.cnn_graph_node import CNNNode
 from nas.composer.cnn_graph import CNNGraph
 from nas.composer.cnn_graph_operator import generate_initial_graph
 from nas.utils.var import TESTING_ROOT
+from nas.cnn_builder import NASDirector, CNNBuilder
+
+requirements = GPNNComposerRequirements(input_shape=[120, 120, 3], pop_size=1,
+                                        num_of_generations=1, max_num_of_conv_layers=4,
+                                        max_nn_depth=3, primary=['conv2d'], secondary=['dense'],
+                                        batch_size=4, epochs=1,
+                                        has_skip_connection=True, skip_connections_id=[0, 2, 5], shortcuts_len=2)
 
 
 def generate_graph():
-    nodes_list = ['conv2d', 'conv2d', 'dropout', 'conv2d', 'flatten', 'dense', 'dropout', 'dense', 'dense']
-    graph = generate_initial_graph(CNNGraph, CNNNode, nodes_list)
+    director = NASDirector()
+    director.set_builder(CNNBuilder(requirements=requirements))
+    graph = director.create_nas_graph()
     return graph
 
 
